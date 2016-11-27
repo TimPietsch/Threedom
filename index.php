a1701e3b309d6f4d29d1c08e365a9dbccf437e18
TimPietsch <TimPietsch@github.com>
2016-11-27 19:24:08 +0100

    updated index.php

--- index.php
+++ index.php
@@ -6,9 +6,13 @@ ini_set('display_errors', 1);
 // Set internal character encoding to UTF-8
 mb_internal_encoding('UTF-8');
 
-// Set autoloader
+// Set root directory
 $root = __DIR__;
+
+// Set include path
 set_include_path($root);
+
+// Set autoloader
 require_once 'Threedom/Core/Routines/Autoload.php';
 
 // Read configuration
