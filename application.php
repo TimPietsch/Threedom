5ba922d281d236cb90b94ccff6cd33d089e7b4e7
TimPietsch <timpietsch.prv@gmail.com>
2016-11-28 19:14:20 +0100

    crude check for deployment

--- application.php
+++ application.php
@@ -18,6 +18,13 @@ require_once 'Threedom/Core/Routines/AutoloadOld.php';
 // Read configuration
 $config = new Threedom\Library\Configuration\Ini($root, 'config.ini');
 
+// Check if the application has been deployed
+if (__FILE__ !== 'index.php') {
+    echo __FILE__;
+}
+
+/* v to be changed v */
+
 // Register directives
 $directives = new Threedom\Core\Directives\Manager();
 include 'directives.php';
