a229276d7f6755b799088b9beeaac4a68592dda6
TimPietsch <timpietsch.prv@gmail.com>
2016-12-01 18:08:24 +0100

    removed config.ini with a temporary fix, Configuration is WIP

---
+++ Temp/config_fix.php
@@ -0,0 +1,4 @@
+<?php
+
+define('SYSTEM_ROOT', $root);
+define('PROJECT_NAME', 'Threedom Test');
--- index.php
+++ index.php
@@ -12,17 +12,20 @@ $root = __DIR__;
 // Set include path
 set_include_path($root);
 
+// Temporary fix while configuration class is WIP
+require_once 'Temp/config_fix.php';
+
 // Set autoloader
 require_once 'Threedom/Core/Routines/Autoload.php';
 spl_autoload_register(new Threedom\Core\Routines\Autoload());
+
+// Read configuration
+//$config = new Threedom\Library\Configuration\Ini($root, 'config.ini');
 
 if (count($_GET) !== 0) {
     // Send POST data to module
 }
 else {
-    // Read configuration
-//    $config = new Threedom\Library\Configuration\Ini($root, 'config.ini');
-
     // Register directives
     $directives = new Threedom\Core\Directives\Manager();
     include 'directives.php';
